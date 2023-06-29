82c0b325f13510ef4c405b9e00318a9820048703
KYU\wjdru <wjdruf23123@gmail.com>
2023-06-29 20:48:02 +0900

    babel/preset runtime 속성 변경( index.js를 제외한 파일에서 import react를 자동화 하기 위해 )

--- source/babel.config.js
+++ source/babel.config.js
@@ -1,27 +1,30 @@
 module.exports = {
-    presets: [
-      [
-        '@babel/preset-env',
-        { modules: false }
-      ],
-      '@babel/preset-react'
+  presets: [
+    [
+      '@babel/preset-env',
+      { modules: false }
     ],
-    plugins: [
-      '@babel/plugin-transform-runtime',
-      '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-proposal-class-properties',
-    ],
-    env: {
-        production: {
-          only: ["src"],
-          plugins: [
-            [
-              "transform-react-remove-prop-types",
-              { removeImport: true } // import PropTypes from 'prop-types' 제거
-            ],
-            "@babel/plugin-transform-react-inline-elements",
-            "@babel/plugin-transform-react-constant-elements"
-          ]
-        }
-      }
+    [ 
+      "@babel/preset-react", 
+      { runtime: "automatic" }
+    ]
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+  ],
+  env: {
+    production: {
+      only: ["src"],
+      plugins: [
+        [
+          "transform-react-remove-prop-types",
+          { removeImport: true } // import PropTypes from 'prop-types' 제거
+        ],
+        "@babel/plugin-transform-react-inline-elements",
+        "@babel/plugin-transform-react-constant-elements"
+      ]
+    }
   }
+}
